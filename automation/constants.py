c0f5be280164aeebc0f56267eaba4b8bad999a46
franz <franzns11@gmail.com>
2024-07-09 10:09:21 +0200

    init

---
+++ automation/constants.py
@@ -0,0 +1,61 @@
+from web3 import Web3
+
+# There should be a pool_config file named $FILE_PREFIX.py in the automation directory that holds the running pool config
+# Outputs will also use this prefix
+FILE_PREFIX = "op_lst_lrt"
+
+CHAIN_NAME = "optimism"
+CURRENT_YEAR = 2024
+# How many incentives should be taken away from vote following to be distributed as fixed incentives?  Per 2 week epoch
+FIXED_INCENTIVE_TOKENS_PER_EPOCH = 1_500
+# Total number of tokens available per 2 week epoch
+TOTAL_TOKENS_PER_EPOCH = 7_520
+DYNAMIC_BOOST_CAP = 3
+MIN_BAL_IN_USD_FOR_BOOST = 200
+TOKENS_TO_FOLLOW_VOTING = TOTAL_TOKENS_PER_EPOCH - FIXED_INCENTIVE_TOKENS_PER_EPOCH
+DESIRED_DEFAULT_VOTE_CAP = 30
+
+BALANCER_GAUGE_CONTROLLER_ABI = [
+    {
+        "stateMutability": "view",
+        "type": "function",
+        "name": "gauge_relative_weight",
+        "inputs": [{"name": "addr", "type": "address"}],
+        "outputs": [{"name": "", "type": "uint256"}],
+    },
+    {
+        "stateMutability": "view",
+        "type": "function",
+        "name": "gauge_relative_weight",
+        "inputs": [
+            {"name": "addr", "type": "address"},
+            {"name": "time", "type": "uint256"},
+        ],
+        "outputs": [{"name": "", "type": "uint256"}],
+    },
+]
+
+# TODO at some point snapshot queries should be moved to bal_tools  also lstGrant.py to think about.
+# Query to fetch pool snapshots
+POOLS_SNAPSHOTS_QUERY = """
+{{
+  poolSnapshots(
+    first: {first}
+    skip: {skip}
+    orderBy: timestamp
+    orderDirection: desc
+    where: {{timestamp_gte: {start_ts}, timestamp_lt: {end_ts}}}
+  ) {{
+    pool {{
+      address
+      id
+      symbol
+    }}
+    timestamp
+    protocolFee
+    swapFees
+    swapVolume
+    liquidity
+  }}
+}}
+"""
---
+++ automation/op_lst_lrt.py
@@ -0,0 +1,95 @@
+from web3 import Web3
+from automation.constants import (
+    FIXED_INCENTIVE_TOKENS_PER_EPOCH,
+    DESIRED_DEFAULT_VOTE_CAP,
+)
+
+#####
+# Notes for the maintainers of this config file
+#####
+# Default cap was set to 10% when this comment was written, will be used if not overridden. (see constants.py)
+# Default fixedBoost is 1 if not specified
+# Default fixexEmissions is 0 if not specified
+# All fixedEmissions should equal up to under constants.py/FIXED_INCENTIVE_TOKENS_PER_EPOCH
+
+# meta information is not required to run, and you can put whatever you want there in json format as notes.
+# gauge
+
+# {
+#         "pool_id": "0x5f8893506ddc4c271837187d14a9c87964a074dc000000000000000000000106",
+#         "meta": {"symbol": "eth-trip", "boostReason": "100% LST"},
+#         "fixedBoost": 1.5,
+#         "capOverride": 20,
+#         "fixedEmissions": 3_000,
+#     },
+
+#  THIS CONFIG IS FOR OPTIMISM CHAIN
+ACTIVE_POOLS_AND_OVERRIDES = [
+    {
+        "pool_id": "0x5f8893506ddc4c271837187d14a9c87964a074dc000000000000000000000106",
+        "meta": {"pool": "eth triplets", "boostReason": "100% LST"},
+        "fixedBoost": 1.5,
+    },
+    {
+        "pool_id": "0x4fd63966879300cafafbb35d157dc5229278ed2300020000000000000000002b",
+        "meta": {"pool": "rocket fuel"},
+    },
+    {
+        "pool_id": "0x7ca75bdea9dede97f8b13c6641b768650cb837820002000000000000000000d5",
+        "meta": {"pool": "gyro wsteth/weth"},
+    },
+    {
+        "pool_id": "0x004700ba0a4f5f22e1e78a277fca55e36f47e09c",
+        "meta": {"pool": "ankr galactic", "boostReason": "100% LST, decentralized LST"},
+        "fixedBoost": 3.25,
+    },
+    {
+        "pool_id": "0xa71021492a3966eec735ed1b505afa097c7cfe6f00000000000000000000010d",
+        "meta": {"pool": "fraximalist sfrxeth/frxeth"},
+    },
+    {
+        "pool_id": "0x2feb76966459d7841fa8a7ed0aa4bf574d6111bf00020000000000000000011d",
+        "meta": {"pool": "yield concerti sfrxeth/sfrax"},
+        "capOverride": 10,
+    },
+    {
+        "pool_id": "0x2a5139cd86c041aa3467e649f5ee0880a5de2f2f00020000000000000000011a",
+        "meta": {"pool": "staked duet stern/wsteth"},
+        "capOverride": 10,
+    },
+    {
+        "pool_id": "0x73a7fe27fe9545d53924e529acf11f3073841b9e000000000000000000000133",
+        "meta": {"pool": "wrsETH/weth", "boostReason": "new to OP"},
+        "fixedBoost": 2,
+    },
+    {
+        "pool_id": "0x2bb4712247d5f451063b5e4f6948abdfb925d93d000000000000000000000136",
+        "meta": {
+            "pool": "stake me baby weeth/wsteth",
+            "boostReason": "new to OP, 100% LST",
+        },
+        "fixedBoost": 3.5,
+    },
+]
+
+
+# assert that the total sum of all fixedEmissions is equal to FIXED_INCENTIVE_TOKENS_PER_EPOCH
+total_fixed_emissions = sum(
+    [x.get("fixedEmissions", 0) for x in ACTIVE_POOLS_AND_OVERRIDES]
+)
+assert (
+    total_fixed_emissions == FIXED_INCENTIVE_TOKENS_PER_EPOCH
+), f"Sum of fixed emissions configured:{total_fixed_emissions} does not equal FIXED_INCENTIVE_TOKENS_PER_EPOCH:{FIXED_INCENTIVE_TOKENS_PER_EPOCH} configured in constants.py"
+
+# Load static boost data
+boost_data = {}
+cap_override_data = {}
+fixed_emissions_per_pool = {}
+# Load static boost here
+for pooldata in ACTIVE_POOLS_AND_OVERRIDES:
+    pool_id_lower = pooldata["pool_id"].lower()
+    boost_data[pool_id_lower] = pooldata.get("fixedBoost", 1)
+    cap_override_data[pool_id_lower] = pooldata.get(
+        "capOverride", DESIRED_DEFAULT_VOTE_CAP
+    )
+    fixed_emissions_per_pool[pool_id_lower] = pooldata.get("fixedEmissions", 0)
